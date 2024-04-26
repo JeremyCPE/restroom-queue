397c4934dbb521d54c439fd31385b1fe1009757d
JeremyCPE <mohariry.jeremy@gmail.com>
2024-04-26 15:09:08 +0900

    (feat) start to implement de first GameHandling

--- src/Urinal.jsx
+++ src/Urinal.jsx
@@ -1,10 +1,12 @@
 import urinal_img from './assets/urinal.png'
+
 function Urinal(props)
 {
     return(
     <div>
         <img className="img" src = "" alt="l"></img>
         <h2>N°{props.id}</h2>
+        <button onClick={() => {props.handleOnClick(props.id)}}>Select</button>
     </div>
     );
 }
--- src/Urinals.jsx
+++ src/Urinals.jsx
@@ -1,16 +1,25 @@
 import Urinal from "./Urinal";
+import React, {useState} from 'react';
+
 
 function Urinals()
 {
+    const[selected, setSelected]=useState();
+
+    const onClickUpdate = (value) =>{
+        console.log("clickUpdated called!")
+        setSelected(value);
+    }
+
     return(
         <>
-        <h1>List : </h1>
+        <h1>List : {selected}</h1>
         <ul className="container">
-            <li className="child"><Urinal id={1}/></li>
-            <li className="child"><Urinal id={2}/></li>
-            <li className="child"><Urinal id={3}/></li>
-            <li className="child"><Urinal id={4}/></li>
-            <li className="child"><Urinal id={5}/></li>
+            <li className="child"><Urinal id={1} handleOnClick = {onClickUpdate}/></li>
+            <li className="child"><Urinal id={2} handleOnClick = {onClickUpdate}/></li>
+            <li className="child"><Urinal id={3} handleOnClick = {onClickUpdate}/></li>
+            <li className="child"><Urinal id={4} handleOnClick = {onClickUpdate}/></li>
+            <li className="child"><Urinal id={5} handleOnClick = {onClickUpdate}/></li>
         </ul>
         </>
     );
